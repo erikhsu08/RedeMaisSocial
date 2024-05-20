e962f17b3d1268e7e6c45360b58409dbf72491f0
MateusKen <54600427+MateusKen@users.noreply.github.com>
2024-05-20 10:44:44 -0300

    Create guiCampanha.java

---
+++ pasta1/guiCampanha.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import ctlCampanha;
+
+public class guiCampanha {
+	Scanner scanner = new Scanner(System.in);
+	int opcao = 0;
+	
+	Sytem.out.println("Por favor, indique se você é: \n(1) Voluntário\n(2) ONG\n(3) Deseja sair.");
+	int ong_ou_voluntario = Scanner.nextInt();
+	
+	//LOGIN
+	System.out.println("LOGIN");
+	String login = scanner.nextLine();
+	System.out.println("Senha");
+	String senha = scanner.nextLine();
+	
+	//VERIFICA SE EXISTE LOGIN
+	
+	do {}while();
+	
+}
