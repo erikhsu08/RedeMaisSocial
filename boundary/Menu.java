394ccf681bce1e3acf0680fd0ff444dbe96d491c
Thiago279 <83241823+Thiago279@users.noreply.github.com>
2024-05-22 16:18:52 -0300

    Update Menu.java

--- boundary/Menu.java
+++ boundary/Menu.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 import Entity.Vaga;
 import Entity.Candidatura;
 import Entity.Campanha;
+import control.CtrlCandidaturas;
 import control.CtrlVagas;
 
 
@@ -14,15 +15,12 @@ public class Menu {
         //Inicialização do Scanner e da variável que guarda a opção do menu
         Scanner scanner = new Scanner(System.in);
         int opcao = 0;
-
-
+        
         CtrlVagas controladorVagas = new CtrlVagas();
-        //LinkedLists utilizadas para armazenar as vagas existentes e candidaturas
+        CtrlCandidaturas conroladorCandidaturas = new CtrlCandidaturas();
 
-        LinkedList <Candidatura> listaCandidaturas = new LinkedList<Candidatura>();
+        //LinkedList <Candidatura> listaCandidaturas = new LinkedList<Candidatura>();
         LinkedList <Campanha> listaCampanhas = new LinkedList<Campanha>();
-
-
 
         //Perguntar se é o usuário é ONG ou Voluntário
         System.out.println("Por favor, indique se você é: \n1.Voluntário\n2.ONG");
@@ -60,21 +58,14 @@ public class Menu {
 
                         break;
                     case 2:
-                        /*System.out.println("Digite o título da vaga que você quer se candidatar: ");
-                        scanner.nextLine();
-                        String titulo = scanner.nextLine();
-                        System.out.println("Digite o nome da ONG: ");
+                        System.out.print("Digite o título da vaga que você quer se candidatar: ");
+                        String titulo =  scanner.nextLine();
+
+                        System.out.print("Digite a ONG responsável: ");
                         String ong = scanner.nextLine();
 
-                        //Validação da escolha de vaga a se candidatar pelo usuário
-                        int flag = 0;
-                        for (int i = 0; i < listaVagas.size(); i++) {
-                            if (listaVagas.get(i).getTitulo().equals(titulo) && listaVagas.get(i).getOng().equals(ong)) {
-                                flag = 1;
-                            }
-                        }
-                        if (flag == 0) {
-                            System.out.println("Vaga não encontrada."); //Mensagem retornada caso a vaga e ong digitada não correspondam
+                        if (! controladorVagas.validaVaga(titulo,ong)) {
+                            System.out.println("Vaga não encontrada."); //Mensagem retornada caso a vaga e ong digitada não correspondam */
                         }else {//Caso correspondam, o processo de candidatura continua
                             System.out.println("Termos e condições: Pelo presente Termo de Adesão e ciente da Lei n. 9.608/1998 que rege o trabalho voluntário, decido espontaneamente\nrealizar atividade voluntária nesta organização. Declaro, ainda, que estou ciente de que o trabalho não será remunerado e que\nnão configurará vínculo empregatício ou gerará qualquer obrigação de natureza trabalhista, previdenciária ou afim. Declaro, por fim, \nque estou ciente de que eventuais danos pessoais ou materiais causados no exercício do trabalho voluntário serão de total e integral responsabilidade minha e não serão imputados à esta organização.\n");
                             System.out.println("Aceita os termos (sim / não): ");
@@ -85,21 +76,21 @@ public class Menu {
                                 System.out.println("Por favor, digite suas informações.");
                                 System.out.println("Nome Completo: ");
                                 String nome = scanner.nextLine();
-                                candidatura.setNome(nome);
+                                //candidatura.setNome(nome);
                                 System.out.println("Data de Nascimento (formato DD/MM/AAAA, com /): ");
                                 String data_nascimento = scanner.nextLine();
-                                candidatura.setData_nascimento(data_nascimento);
+                                //candidatura.setData_nascimento(data_nascimento);
                                 System.out.println("Descrição (um breve resumo sobre você e motivação de sua candidatura): ");
                                 String descricao = scanner.nextLine();
-                                candidatura.setDescricao(descricao);
-
-                                listaCandidaturas.add(candidatura); //Adiciona a candidatura criada na lista de candidaturas
+                                //candidatura.setDescricao(descricao);
+                                conroladorCandidaturas.incrementaFormulario(nome,data_nascimento,descricao);
+                                //listaCandidaturas.add(candidatura); //Adiciona a candidatura criada na lista de candidaturas
                                 System.out.println("Candidatura realizada com sucesso!");
                             } else {//Caso o usuário não concorde com os termos, o processo de candidatura é finalizado e é voltado para o menu de opções
                                 break;
                             }
                         }
-                        break;*/
+                        break;
                 } //fim do switch case
             } while (opcao != 3);
         }else if(ong_ou_voluntario == 2) { //Menu caso o usuário seja ONG
